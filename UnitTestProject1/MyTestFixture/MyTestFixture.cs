edcb2b77fe0874f0392d4d0b3c8b4a8e8c41b0b6
Maxim Ilyukhin <max86work@gmail.com>
2018-07-25 12:07:07 +0300

    add nunit one more

---
+++ UnitTestProject1/MyTestFixture/MyTestFixture.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class MyTestFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

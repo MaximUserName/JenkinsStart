8f89a1df817f79a881993b213892803ab1585f13
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 01:22:04 +0300

    valueseeeee

--- myapi/Controllers/ValuesController.cs
+++ myapi/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace myapi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4", "value5", "value77777"};
+            return new string[] { "value1888888", "value2", "value3", "value4", "value5", "value77777"};
         }
 
         // GET api/values/5

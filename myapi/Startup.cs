1fba4aaf005be082ed1f2288659f585b2fceaf69
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 01:49:12 +0300

    swagger

--- myapi/Startup.cs
+++ myapi/Startup.cs
@@ -36,6 +36,8 @@ namespace myapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

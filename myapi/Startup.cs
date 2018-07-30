50cd65ddc20b514886384c49dcdc2788a28b0052
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 01:40:41 +0300

    valueseeeee

--- myapi/Startup.cs
+++ myapi/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.Swagger;
 
 namespace myapi
 {

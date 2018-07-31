500d4d3bb401c60493162276c4b5fa4d7af3e946
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 03:18:14 +0300

    postgres1

---
+++ myapi/Controllers/BlogContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myapi.Controllers
+{
+    public class BlogContext : DbContext
+    {
+        // When used with ASP.net core, add these lines to Startup.cs
+        //   var connectionString = Configuration.GetConnectionString("BlogContext");
+        //   services.AddEntityFrameworkNpgsql().AddDbContext<BlogContext>(options => options.UseNpgsql(connectionString));
+        // and add this to appSettings.json
+        // "ConnectionStrings": { "BlogContext": "Server=localhost;Database=blog" }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString =
+                "Server=postgre-test-db.c2sxohx0wlme.eu-west-3.rds.amazonaws.com;Port=5432;Database=catalogdb1;User Id=masterlogin;Password=*fzRFz2?;";
+
+            optionsBuilder.UseNpgsql(connectionString);
+            //  base.OnConfiguring(optionsBuilder);
+        }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}

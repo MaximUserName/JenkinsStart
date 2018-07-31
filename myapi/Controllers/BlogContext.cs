e8309b06605783a76dbccae8b9fc7e65b1133612
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 03:50:04 +0300

    postgres

--- myapi/Controllers/BlogContext.cs
+++ myapi/Controllers/BlogContext.cs
@@ -10,15 +10,15 @@ namespace myapi.Controllers
         //   services.AddEntityFrameworkNpgsql().AddDbContext<BlogContext>(options => options.UseNpgsql(connectionString));
         // and add this to appSettings.json
         // "ConnectionStrings": { "BlogContext": "Server=localhost;Database=blog" }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString =
-                "Server=postgre-test-db.c2sxohx0wlme.eu-west-3.rds.amazonaws.com;Port=5432;Database=catalogdb1;User Id=masterlogin;Password=*fzRFz2?;";
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var connectionString =
+        //        "Server=postgre-test-db.c2sxohx0wlme.eu-west-3.rds.amazonaws.com;Port=5432;Database=catalogdb1;User Id=masterlogin;Password=*fzRFz2?;";
 
-            optionsBuilder.UseNpgsql(connectionString, builder =>
-                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null));
-            base.OnConfiguring(optionsBuilder);
-        }
+        //    optionsBuilder.UseNpgsql(connectionString, builder =>
+        //        builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null));
+        //    base.OnConfiguring(optionsBuilder);
+        //}
 
         public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
 

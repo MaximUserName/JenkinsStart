270d1f84a1db42fea4a325c19a17fed1dbaad486
Maxim Ilyukhin <max86work@gmail.com>
2018-07-31 12:33:04 +0300

    postgres

--- myapi/Controllers/TasksController.cs
+++ myapi/Controllers/TasksController.cs
@@ -2,28 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace myapi.Controllers
 {
     [Route("api/[controller]")]
+    public class SystemController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public SystemController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_environment);
+        }
+    }
+
+    [Route("api/[controller]")]
     public class TasksController : Controller
     {
         private readonly BlogContext _context;
+        private readonly IHostingEnvironment _environment;
 
-        public TasksController(BlogContext context)
+        public TasksController(BlogContext context, IHostingEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
         // GET api/values
         [HttpGet]
         public IActionResult Get()
         {
+            //return Ok(_environment);
             //List<Task> tasks = new List<Task>();
             try
             {
                var tasks = _context.Tasks.ToArray();
-                return Ok(_context.Tasks);
+                return Ok(tasks);
             }
             catch (Exception e)
             {
--- myapi/Startup.cs
+++ myapi/Startup.cs
@@ -44,7 +44,7 @@ namespace myapi
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, BlogContext context)
         {
             // https://abelsquidhead.com/index.php/2017/07/31/deploying-dbs-in-your-cicd-pipeline-with-ef-core-code-first/
-            context.Database.Migrate();
+            // context.Database.Migrate();
 
             app.UseStaticFiles();
             app.UseSwagger();
